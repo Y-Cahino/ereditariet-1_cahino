da4c546feb721bc68f5260963296dd31ae55823f
Yan Nascimento <yanzebedeu@hotmail.com>
2023-09-28 10:52:29 +0200

    Aggiungere i file di progetto.

---
+++ ereditarietà1_cahino/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ereditarietà1_cahino
+{
+    class Poligoni
+    {
+        private double lato;
+        private double alt;
+        private double lato2;
+        private double lato3;
+        class Quadrato:Poligoni
+        {
+            public Quadrato()
+            {
+                lato = 0.0;
+            }
+            public Quadrato(double lato)
+            {
+                setl(lato);
+            }
+        }
+    public void perimetro(double p)
+    {
+        p = lato * 4;
+    }
+    public double getl()
+    {
+        return lato;
+    }
+    public double getl2()
+    {
+        return lato;
+    }
+    public double getl3()
+    {
+        return lato;
+    }
+    public double geth()
+    {
+        return alt;
+    }
+    public void seth(double h)
+    {
+        if (h > 0)
+        {
+            alt = h;
+        }
+    }
+    public void setl(double l)
+    {
+        if (l > 0) lato = l;
+    }
+}
+    }
+}
+
+
---
+++ ereditarietà1_cahino/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ereditarietà1_cahino
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
